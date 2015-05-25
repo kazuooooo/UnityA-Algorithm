c95195bd3e686fd3f9da759bec16f88977d025e4
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-05-25 18:02:17 +0900

    map create

---
+++ UnityA-StarAlgorithm/Assets/scripts/Stage/StageCreator.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageCreator : MonoBehaviour
+{
+		//配置するオブジェクトの一覧
+		public GameObject[] tileObjects;
+		//タイル1枚あたりの高さ
+		private float tileXWidth;
+		//タイル1枚あたりの幅
+		private float tileZWidth;
+		//タイル情報と番号を紐付けたhashtable
+		private Hashtable tileHash;
+		private StageTextDataAnalyzer textAnalyzerClass;
+		public TextAsset[] stageText;
+		private GameObject stageObj;
+
+		void Awake ()
+		{
+				//stageObjを取得
+				stageObj = GameObject.Find ("Stage") as GameObject;
+				//stageTextDataAnalyzerクラスを取得
+				textAnalyzerClass = gameObject.GetComponent<StageTextDataAnalyzer> ();
+				//タイルのx方向の大きさを取っておく
+				tileZWidth = tileObjects [0].gameObject.renderer.bounds.size.z;
+				//タイルのY方向の大きさ取っておく
+				tileXWidth = tileObjects [0].gameObject.renderer.bounds.size.x;
+				//Hashテーブルを作成
+				tileHash = new Hashtable ();
+				//hash(ID,Object)
+				tileHash.Add (1, tileObjects [0]);
+				tileHash.Add (2, tileObjects [1]);
+				tileHash.Add (3, tileObjects [2]);
+				tileHash.Add (4, tileObjects [3]);
+				tileHash.Add (5, tileObjects [4]);
+				tileHash.Add (6, tileObjects [5]);
+				tileHash.Add (7, tileObjects [6]);
+				tileHash.Add (8, tileObjects [7]);
+				tileHash.Add (9, tileObjects [8]);
+				tileHash.Add (10, tileObjects [9]);
+
+				makeStage (1);
+		}
+
+		public void makeStage (int stageNum)
+		{
+				//指定したステージ番号のステージのanalyzeDataを取得
+				int[,] analyzedTextData = textAnalyzerClass.GetTileData (stageText [stageNum - 1]);
+				//ステージのX方向、Z方向の幅を取得
+				int stageZWidth = analyzedTextData.GetLength (0);
+				int stageXWidth = analyzedTextData.GetLength (1);
+				//blockObject
+				GameObject blockObj = null;
+				//幅-1が0,0から始まる
+				int tileZ = stageZWidth - 1;
+				int tileX = stageXWidth - 1;
+				float tilePosX = 0;
+				float tilePosY = 0;
+				float tilePosZ = 0;
+
+
+
+				//パネルをずらして順番に設置
+				for (int z = 1; z <= stageZWidth; z++) {
+						for (int x = 1; x <= stageXWidth; x++) {
+								print ("panel_x" + tileX + "panel_z" + tileZ);
+								blockObj = Instantiate (tileHash [analyzedTextData [tileZ, tileX]]as GameObject, new Vector3 (tilePosX, tilePosY, tilePosZ), Quaternion.identity) as GameObject;
+								blockObj.transform.SetParent (stageObj.transform, false);
+								//座標をx方向に１つずらす
+								tileX--;
+								//Xのpositionを幅分ずらす
+								tilePosX += tileXWidth;
+						}
+						//座標をz方向に１つずらす
+						tileZ--;
+						//Zのpositionを幅分ずらす
+						tilePosZ += tileZWidth;
+						//x座標を初期値に戻す
+						tileX = stageXWidth - 1;
+						//Xのpositionを初期値に戻す
+						tilePosX = 0;
+
+				}
+		}
+}
---
+++ UnityA-StarAlgorithm/Assets/scripts/Stage/StageTextDataAnalyzer.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StageTextDataAnalyzer : MonoBehaviour
+{
+		//生のテキスト
+		string stageText;
+		//ステージを業単位に分解したもの
+		string[] lines;
+		//ステージの高さ
+		private int stageHeight;
+		//ステージの幅
+		private int stageWidth;
+		int[,] tileData;
+		int[,] tileDataSend;
+		int num_x = 0;
+		int num_y = 0;
+		public int[,] testData;
+		//取得した行を入れるリスト
+		// Use this for initialization
+		//タイルデータを２次元配列で取得
+		public int[,] GetTileData (TextAsset stageDataText)
+		{
+				//文字列を取得
+				stageText = stageDataText.text;
+				//文字列を業単位に分割
+				lines = stageText.Split ('\n');
+				//ステージの高さ、幅を取得
+				stageWidth = GetParam ("width");
+				stageHeight = GetParam ("height");
+				List<string> tileLineDataList = new List<string> ();
+				int[,] tileData = new int[stageWidth, stageHeight];
+
+       
+				//データの始まりの行を取得
+				int dataLineNum = System.Array.IndexOf (lines, "type=GroundLayer") + 2;
+
+
+				//空白になるまで
+				while (lines [dataLineNum] != "") {
+						//行ごとのデータを取得してリストに格納
+						tileLineDataList.Add (lines [dataLineNum]);
+						dataLineNum++;
+				}
+
+				foreach (string tileLine in tileLineDataList) {
+						//カンマで区切る
+						string[] tile_list = tileLine.Split (',');
+
+						//区切った数字を２次元配列に格納
+						foreach (string tile in tile_list) {
+
+								if (tile == "") {
+										num_x = 0;
+										num_y++;
+								}
+
+								if (tile != "") {
+										tileData [num_x, num_y] = int.Parse (tile);
+										// Debug.Log ("(" + num_x + "," + num_y + ")" + tile);
+										num_x++;
+								}
+
+
+						}
+				}
+
+				return tileData;
+            
+		}
+		//必要なパラメータを取得して返す(ex)height = 10みたいなものに使用)
+		private int GetParam (string requisiteData)
+		{
+				int param = 0;
+				//読み込み
+				foreach (string line in lines) {
+						if (line.StartsWith (requisiteData)) {
+								string[] words = line.Split ('=');
+								param = int.Parse (words [1]);
+								// Debug.Log (param + param);
+						}
+				}
+				return param;
+		}
+		//デバッグ用
+		private void printAllData ()
+		{
+				foreach (string line in lines) {
+						Debug.Log (line);
+				}
+		}
+
+		public T GetAtIndex<T> (T[] pArray, int i)
+		{
+				return pArray [i];
+		}
+}
